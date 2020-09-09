cc2fb287ddd6c40714bb27fcbe14407d73846acf
Dawoox <dawoox.contact@gmail.com>
2020-09-09 13:19:47 +0200

    Add music fonctions and reactive functions

--- src/main/java/fr/dawoox/yua/Yua.java
+++ src/main/java/fr/dawoox/yua/Yua.java
@@ -1,12 +1,23 @@
 package fr.dawoox.yua;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.voice.AudioProvider;
 import fr.dawoox.yua.commands.Command;
+import fr.dawoox.yua.commands.music.LavaPlayerAudioProvider;
+import fr.dawoox.yua.commands.music.TrackScheduler;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,18 +26,53 @@ public class Yua {
     private static final Map<String, Command> commands = new HashMap<>();
 
     public static void main(String[] args) {
+
         final String token = args[0];
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient g = client.login().block();
 
-        g.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            if ("!ping".equalsIgnoreCase(message.getContent())) {
-                final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
-            }
-        });
+        g.getEventDispatcher().on(MessageCreateEvent.class)
+                // 3.1 Message.getContent() is a String
+                .flatMap(event -> Mono.just(event.getMessage().getContent())
+                        .flatMap(content -> Flux.fromIterable(commands.entrySet())
+                                // We will be using ! as our "prefix" to any command in the system.
+                                .filter(entry -> content.startsWith('!' + entry.getKey()))
+                                .flatMap(entry -> entry.getValue().execute(event))
+                                .next()))
+                .subscribe();
 
         g.onDisconnect().block();
+
+    }
+
+    static {
+
+        // Creates AudioPlayer instances and translates URLs to AudioTrack instances
+        final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
+        // This is an optimization strategy that Discord4J can utilize. It is not important to understand
+        playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+        // Allow playerManager to parse remote sources like YouTube links
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        // Create an AudioPlayer so Discord4J can receive audio data
+        final AudioPlayer player = playerManager.createPlayer();
+        // We will be creating LavaPlayerAudioProvider in the next step
+        AudioProvider provider = new LavaPlayerAudioProvider(player);
+
+        commands.put("ping", event -> event.getMessage().getChannel()
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .then());
+        commands.put("join", event -> Mono.justOrEmpty(event.getMember())
+                .flatMap(Member::getVoiceState)
+                .flatMap(VoiceState::getChannel)
+                // join returns a VoiceConnection which would be required if we were
+                // adding disconnection features, but for now we are just ignoring it.
+                .flatMap(channel -> channel.join(spec -> spec.setProvider(provider)))
+                .then());
+        final TrackScheduler scheduler = new TrackScheduler(player);
+        commands.put("play", event -> Mono.justOrEmpty(event.getMessage().getContent())
+                .map(content -> Arrays.asList(content.split(" ")))
+                .doOnNext(command -> playerManager.loadItem(command.get(1), scheduler))
+                .then());
+
     }
 }
--- src/main/java/fr/dawoox/yua/commands/Command.java
+++ src/main/java/fr/dawoox/yua/commands/Command.java
@@ -1,7 +1,10 @@
 package fr.dawoox.yua.commands;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Mono;
 
 public interface Command {
-    void execute(MessageCreateEvent event);
+    // Since we are expecting to do reactive things in this method, like
+    // send a message, then this method will also return a reactive type.
+    Mono<Void> execute(MessageCreateEvent event);
 }
