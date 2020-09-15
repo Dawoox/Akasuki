ebef48b59072ffc805e66d5f41f90c57a2486222
Antoine Candelon <dawoox.contact@gmail.com>
2020-09-15 21:36:14 +0200

    Ping.java finished.

--- src/main/java/fr/dawoox/yua/commands/misc/Ping.java
+++ src/main/java/fr/dawoox/yua/commands/misc/Ping.java
@@ -1,15 +1,23 @@
 package fr.dawoox.yua.commands.misc;
 
 import fr.dawoox.yua.utils.Command;
+import fr.dawoox.yua.utils.TimeManager;
 
+import java.time.Instant;
 import java.util.Map;
 
 public class Ping {
 
     public static void reg(Map<String, Command> commands){
-        commands.put("ping", event -> event.getMessage()
-                .getChannel().block()
-                .createMessage("Pong!").block());
+        commands.put("ping", event -> {
+            Instant messageInstant = event.getMessage().getTimestamp();
+            event.getMessage().getChannel().block().createMessage("Pinging...");
+            Instant botInstant = Instant.now();
+            String reply = "La latence de Yua est de `" + TimeManager.diffInMillis(messageInstant, botInstant) + "ms` actuellement" +
+                    "\nUn problème de latence ? Venez nous l'indiquer sur notre Discord de support";
+            event.getMessage().getChannel().block().getLastMessage().block().delete("Yua auto ping message deleted");
+            event.getMessage().getChannel().block().createMessage(reply).block();
+        });
     }
 
 }
