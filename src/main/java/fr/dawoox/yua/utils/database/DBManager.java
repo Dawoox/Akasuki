447623c5c1f23c4ff9c068a908bc74d1a78d58bb
Antoine Candelon <dawoox.contact@gmail.com>
2020-09-24 22:17:45 +0200

    Working version, fr.dawoox.yua.modules system reported, fix DBManager.java

--- src/main/java/fr/dawoox/yua/utils/database/DBManager.java
+++ src/main/java/fr/dawoox/yua/utils/database/DBManager.java
@@ -11,7 +11,7 @@ public class DBManager {
     private static final String USER = args[0];
     private static final String PASSWORD = args[1];
 
-    private static final MongoClient mongoClient = MongoClients.create("mongodb+srv://" + USER + ":" + "" + "@acme.ypiul.gcp.mongodb.net/yua?retryWrites=true&w=majority");
+    private static final MongoClient mongoClient = MongoClients.create("mongodb+srv://" + USER + ":" + PASSWORD + "@acme.ypiul.gcp.mongodb.net/yua?retryWrites=true&w=majority");
     private static final MongoDatabase database = mongoClient.getDatabase("yua");
     public static MongoDatabase getDatabase() { return database; }
     public static boolean isLink() { if (database == null) { return false; } else { return true; } }
