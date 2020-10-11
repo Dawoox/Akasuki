9277afadf9bad58a433b3c9a502b4ddb10a205cc
Dawoox <candelon.antoine@gmail.com>
2020-10-11 15:56:13 +0200

    Add javadoc to test classes

--- src/test/java/fr/dawoox/akasuki/utils/TestTimeUtils.java
+++ src/test/java/fr/dawoox/akasuki/utils/TestTimeUtils.java
@@ -8,13 +8,26 @@ import static fr.dawoox.akasuki.utils.TimeUtils.diffInDays;
 import static fr.dawoox.akasuki.utils.TimeUtils.diffInMillis;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestTimeManager {
+/**
+ * Testing class for TimeUtils
+ * @author Dawoox
+ * @version 1.1.0
+ */
+public class TestTimeUtils {
 
+    /**
+     * testDiffInDays test methode
+     * @since 1.0.0
+     */
     @Test
     public void testDiffInDays() {
         assertEquals(0, diffInDays(Instant.now(), Instant.now()));
     }
 
+    /**
+     * testDiffInMillis test methode
+     * @since 1.1.0
+     */
     @Test
     public void testDiffInMillis() {
         assertEquals(0, diffInMillis(Instant.now(), Instant.now()));
--- src/test/java/fr/dawoox/akasuki/utils/TestUrlChecker.java
+++ src/test/java/fr/dawoox/akasuki/utils/TestUrlChecker.java
@@ -4,8 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Testing class for UrlChecker
+ * @author Dawoox
+ * @version 1.0.0
+ */
 public class TestUrlChecker {
 
+    /**
+     * isValid test methode
+     * @since 1.0.0
+     */
     @Test
     public void testIsValid(){
         assertTrue(UrlChecker.isValid("https://google.fr"));
