80558b0565e5077ecc612233de67815bb6f29ef1
Dawoox <dawoox.contact@gmail.com>
2021-01-27 22:35:49 +0100

    Activity System

--- src/main/java/fr/dawoox/akasuki/Akasuki.java
+++ src/main/java/fr/dawoox/akasuki/Akasuki.java
@@ -5,14 +5,12 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.presence.Activity;
-import discord4j.core.object.presence.Presence;
 import discord4j.rest.response.ResponseFunction;
 import fr.dawoox.akasuki.core.command.MessageProcessor;
+import fr.dawoox.akasuki.core.thread.ActivityManager;
 import fr.dawoox.akasuki.data.Config;
 import io.prometheus.client.exporter.HTTPServer;
 import io.sentry.Sentry;
-import org.slf4j.LoggerFactory;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
@@ -40,7 +38,6 @@ public class Akasuki {
     public static void main(String[] args) {
         //Set default locale to FR
         Locale.setDefault(Locale.FRANCE);
-
         DEFAULT_LOGGER.info(Figlet.render());
 
         try {
@@ -52,36 +49,22 @@ public class Akasuki {
             Sentry.init(sentryOptions -> sentryOptions.setDsn("https://8f8f812b07284a7b99b8527cb2b94839@o473268.ingest.sentry.io/5508041"));
         }
 
-        DEFAULT_LOGGER.info("Initializing ");
-
+        DEFAULT_LOGGER.info("Initializing");
         final DiscordClient client = DiscordClient.builder(Config.TOKEN).onClientResponse(ResponseFunction.emptyIfNotFound()).build();
-
         Akasuki.owner_id = Snowflake.of(client.getApplicationInfo().block().owner().id());
 
         DEFAULT_LOGGER.info("Connecting to Discord");
-
         final GatewayDiscordClient gateway = client.login().block();
-
         System.setProperty("http.agent", Config.USER_AGENT);
         assert gateway != null;
 
-        //Get call when a new message is send in any guild where the bot is
         gateway.getEventDispatcher().on(MessageCreateEvent.class).subscribe(MessageProcessor::processEvent);
 
-        //Get call when the bot start
+        DEFAULT_LOGGER.info("Updating bot activity");
         gateway.getEventDispatcher().on(ReadyEvent.class)
                 .subscribe(readyEvent -> {
                     guildCounts = gateway.getGuilds().collectList().block().size();
-                    //Update status if present.
-                    if (args.length >= 1){
-                        LoggerFactory.getLogger(Akasuki.class).info("Changing Activity...");
-                        String activity = "";
-                        for (int i=0;i<args.length;i++){
-                            activity = activity += args[i] + " ";
-                        }
-                        gateway.updatePresence(Presence.online(Activity.watching(activity))).block();
-                        LoggerFactory.getLogger(Akasuki.class).info("Activity Changed");
-                    }
+                    new ActivityManager().run(gateway);
                 });
 
         gateway.onDisconnect().block();
@@ -90,7 +73,6 @@ public class Akasuki {
     public static Snowflake getOwnerId() {
         return owner_id;
     }
-
     public static long getGuildCount() {
         return guildCounts;
     }
