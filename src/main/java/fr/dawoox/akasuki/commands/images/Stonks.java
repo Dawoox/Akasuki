350aed4ac0e39840e97271ed3bc9de1ca231b6ef
Dawoox <dawoox.contact@gmail.com>
2021-02-07 22:31:02 +0100

    Stonsk re-work

--- src/main/java/fr/dawoox/akasuki/commands/images/Stonks.java
+++ src/main/java/fr/dawoox/akasuki/commands/images/Stonks.java
@@ -1,46 +1,51 @@
 package fr.dawoox.akasuki.commands.images;
 
+import com.sun.tools.javac.util.List;
 import fr.dawoox.akasuki.core.command.BaseCmd;
+import fr.dawoox.akasuki.core.command.CommandCategory;
+import fr.dawoox.akasuki.core.command.CommandPermission;
+import fr.dawoox.akasuki.core.command.Context;
 import fr.dawoox.akasuki.utils.ImageUtils;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
 
 import java.awt.*;
-import java.util.Map;
 
 /**
  * Show a image of a stonks templated with the sender profil image into /!\ IN BETA /!\
  * @author Dawoox
  * @version 1.3.0
  */
-public class Stonks {
+public class Stonks extends BaseCmd {
 
-    /*
-    public static void reg(Map<String, BaseCmd> commands) {
-        commands.put("stonks", event -> {
-            ImagePlus font = IJ.openImage("src\\main\\java\\fr\\dawoox\\akasuki\\commands\\images\\stonks.png");
-            ImagePlus avatar = IJ.openImage(event.getMessage().getAuthor().get().getAvatarUrl());
-            ImageProcessor ip = font.getProcessor();
+    public Stonks() {
+        super(CommandCategory.IMAGE, CommandPermission.USER, List.of("stonks"));
+    }
 
-            try {
-                for (int i=0;i<avatar.getWidth();i++){
-                    for (int i2=0;i2<avatar.getHeight();i2++){
-                        int[] temp = avatar.getPixel(i, i2);
-                        Color color = new Color(temp[0], temp[1], temp[2]);
-                        ip.setColor(color);
-                        ip.setLineWidth(1);
-                        if (!color.equals(Color.BLACK)){
-                            ip.drawPixel(i + 125, i2 + 50);
-                        }
+    @Override
+    public void execute(Context context) {
+        ImagePlus font = IJ.openImage("src\\main\\java\\fr\\dawoox\\akasuki\\commands\\images\\stonks.png");
+        ImagePlus avatar = IJ.openImage(context.getAuthor().getAvatarUrl());
+        ImageProcessor ip = font.getProcessor();
+
+        try {
+            for (int i=0;i<avatar.getWidth();i++){
+                for (int i2=0;i2<avatar.getHeight();i2++){
+                    int[] temp = avatar.getPixel(i, i2);
+                    Color color = new Color(temp[0], temp[1], temp[2]);
+                    ip.setColor(color);
+                    ip.setLineWidth(1);
+                    if (!color.equals(Color.BLACK)){
+                        ip.drawPixel(i + 125, i2 + 50);
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            event.getMessage().getChannel().block().createMessage(MessageCreateSpec ->
-                    MessageCreateSpec.addFile("stonks.png", ImageUtils.getDiscordCompatibleFile(font))).block();
-        });
-    }*/
+        context.getChannel().createMessage(MessageCreateSpec ->
+                MessageCreateSpec.addFile("stonks.png", ImageUtils.getDiscordCompatibleFile(font))).block();
+    }
 }
