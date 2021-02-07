181e2352ba4e527109aca38c66f2ad286050a35d
Dawoox <dawoox.contact@gmail.com>
2021-02-07 22:30:53 +0100

    Kiss re-work

--- src/main/java/fr/dawoox/akasuki/commands/gifs/Kiss.java
+++ src/main/java/fr/dawoox/akasuki/commands/gifs/Kiss.java
@@ -1,36 +1,54 @@
 package fr.dawoox.akasuki.commands.gifs;
 
+import at.mukprojects.giphy4j.Giphy;
+import at.mukprojects.giphy4j.exception.GiphyException;
+import com.sun.tools.javac.util.List;
 import discord4j.core.object.entity.Member;
+import discord4j.rest.util.Color;
+import fr.dawoox.akasuki.core.command.BaseCmd;
+import fr.dawoox.akasuki.core.command.CommandCategory;
+import fr.dawoox.akasuki.core.command.CommandPermission;
+import fr.dawoox.akasuki.core.command.Context;
+import fr.dawoox.akasuki.data.Config;
+import io.sentry.Sentry;
+
+import java.time.Instant;
 
 /**
  * Show a random kiss gif
  * @author Dawoox
  * @version 1.1.0
  */
-public class Kiss {
+public class Kiss extends BaseCmd {
 
-    private static Member target;
-    private static String reply = "default error";
+    private static String reply = "uh, is that a error?";
 
-    /*
-    public static void reg(Map<String, BaseCmd> commands){
-        commands.put("kiss", event -> {
-            MessageChannel channel = event.getMessage().getChannel().block();
-            Member sender = event.getMessage().getAuthorAsMember().block();
+    public Kiss() {
+        super(CommandCategory.FUN, CommandPermission.USER, List.of("kiss"));
+    }
 
-            assert sender != null;
-            assert channel != null;
+    @Override
+    public void execute(Context context) {
+        Giphy giphy = new Giphy(Config.GIPHY_API_KEY);
 
-            if (!event.getMessage().getUserMentionIds().isEmpty()){
-                Kiss.target = Objects.requireNonNull(event.getMessage().getUserMentions().blockFirst()).asMember(event.getGuildId().get()).block();
-                reply = sender.getUsername() + " embrasse " + target.getUsername();
-            } else {
-                reply = sender.getUsername() + " embrasse quelqu'un";
+        if (!context.getMessage().getUserMentionIds().isEmpty()) {
+            reply = context.getAuthor().getUsername()+" kiss  "+context.getMessage().getUserMentions().blockFirst().asMember(context.getGuildId()).block();
+        } else {
+            reply = context.getAuthor().getUsername()+" kiss";
+        }
+
+        context.getChannel().createEmbed( embedCreateSpec -> {
+            try {
+                embedCreateSpec.setColor(Color.DEEP_LILAC)
+                        .setAuthor(reply, null, null)
+                        .setImage(giphy.searchRandom("kiss").getData().getImageOriginalUrl())
+                        .setTimestamp(Instant.now())
+                        .setFooter("Akasuki", null);
+            } catch (GiphyException e) {
+                Sentry.captureException(e);
+                e.printStackTrace();
             }
-
-            EmbedTemplate.sendEmbed(channel, reply, "kiss");
-            LogsWriter.logAction("Kiss : ", sender, Kiss.class);
-        });
-    }*/
+        }).block();
+    }
 
 }
